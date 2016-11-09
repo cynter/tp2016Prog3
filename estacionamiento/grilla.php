e72bd493791255b64f84b6c85ffe7d9f65724dba
cynter <cynteran27@gmail.com>
2016-11-08 21:20:55 -0300

    grillas

--- estacionamiento/grilla.php
+++ estacionamiento/grilla.php
@@ -6,34 +6,41 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<link rel="stylesheet" href="css/login.css">
+	<link rel="stylesheet" href="css/login.css">
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-	<a href="#" id="verGrillaA">Autos estacionados</a>
-	<br>
-	<a href="#" id="verGrillaB">Importes cobrados por autos</a>	
-	<br>
-	<a href="#" id="verGrillaC">Usuarios</a>	
+	<div class="container">
+        <div class="card card-container">
 
-	<?php if(isset($_GET['importe'])) { ?>
-		<p style="display: none" id="importe"><?php echo $_GET['importe']; ?></p>
-		<script type="text/javascript">
-			var importe = $('#importe').html();
-			alert('El importe a pagar es :' + importe);
-		</script>
-	<?php } ?>
+			<button id="verGrillaA" class="btn btn-lg btn-primary btn-block" type="button" >Autos estacionados</button>
+			<button id="verGrillaB" class="btn btn-lg btn-primary btn-block" type="button" >Importes cobrados por autos</button>
+			<button id="verGrillaC" class="btn btn-lg btn-primary btn-block" type="button" >Usuarios</button>
 
-	<?php if(isset($_GET['error'])) { ?>
-		<p style="display: none" id="mensaje"><?php echo $_GET['error']; ?></p>
-		<script type="text/javascript">
-			var mensaje = $('#mensaje').html();
-			alert('No existe el auto!!');
-		</script>
-	<?php } ?>
 
-	<table id="grilla"></table>
+		<?php if(isset($_GET['importe'])) { ?>
+			<p style="display: none" id="importe"><?php echo $_GET['importe']; ?></p>
+			<script type="text/javascript">
+				var importe = $('#importe').html();
+				alert('El importe a pagar es :' + importe);
+			</script>
+		<?php } ?>
+
+		<?php if(isset($_GET['error'])) { ?>
+			<p style="display: none" id="mensaje"><?php echo $_GET['error']; ?></p>
+			<script type="text/javascript">
+				var mensaje = $('#mensaje').html();
+				alert('No existe el auto!!');
+			</script>
+		<?php } ?>
+
+
+			
+			<br />
+					<table id="grilla"></table>
+        </div><!-- /card-container -->
+    </div><!-- /container -->
 
 	<script src="js/funciones.js"></script>
 </body>
--- estacionamiento/index.php
+++ estacionamiento/index.php
@@ -5,19 +5,33 @@
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+	<link rel="stylesheet" href="css/login.css">
+	
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-	<?php if($_GET['is_admin']) { ?>
-		<a href="grilla.php">Ver grillas</a>
-	<?php } ?>
-	<br>
-	<a href="ingresarPatente.php">Ingresar patente</a>
-	<br>
-	<a href="sacarPatente.php">Sacar patente</a>
+
+<div class="container">
+        <div class="card card-container">
+<!--
+			<button id="verGrillaA" class="btn btn-lg btn-primary btn-block" type="button" >Autos estacionados</button>
+			<button id="verGrillaB" class="btn btn-lg btn-primary btn-block" type="button" >Importes cobrados por autos</button>
+			<button id="verGrillaC" class="btn btn-lg btn-primary btn-block" type="button" >Usuarios</button>
+-->
+			<?php if($_GET['is_admin']) { ?>
+				<a href="grilla.php">Ver grillas</a>
+			<?php } ?>
+			<br>
+			<a href="ingresarPatente.php">Ingresar patente</a>
+			<br>
+			<a href="sacarPatente.php">Sacar patente</a>
+
+	        </div><!-- /card-container -->
+    </div><!-- /container -->
+
 
 	<?php if(isset($_GET['importe'])) { ?>
 		<p style="display: none" id="importe"><?php echo $_GET['importe']; ?></p>
